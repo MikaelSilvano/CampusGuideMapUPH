6492f34a6ffb4f88a9cdee021dd89a009d2bdd5f
Mikael Silvano <mikaelsilvano04@gmail.com>
2025-11-01 21:05:51 +0700

    1. Fix sentences in AuthTokenProvider.kt

--- CampusGuideMapUPH/app/src/main/java/com/example/campusguide/data/remote/AuthTokenProvider.kt
+++ CampusGuideMapUPH/app/src/main/java/com/example/campusguide/data/remote/AuthTokenProvider.kt
@@ -5,7 +5,7 @@ import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.tasks.await
 
 suspend fun getIdTokenOrThrow(): String {
-    val user = Firebase.auth.currentUser ?: error("User belum login")
+    val user = Firebase.auth.currentUser ?: error("User has not login")
     val token = user.getIdToken(true).await().token
-    return token ?: error("Gagal ambil ID token")
+    return token ?: error("Failed to take token ID")
 }
