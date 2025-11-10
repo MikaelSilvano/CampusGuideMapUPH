4e44ab24ccf2104309528a35586a283e695d6802
Corvus2211 <bryansusantoyo@gmail.com>
2025-11-10 10:10:32 +0700

    removing the need of service account in granting admin

--- CampusGuideMapUPH/backend/grant-admin.js
+++ CampusGuideMapUPH/backend/grant-admin.js
@@ -1,16 +1,16 @@
 const path = require('path');
 const admin = require('firebase-admin');
 
-const keyPath = path.join(__dirname, 'keys', 'campus-guide-map-uph-firebase-adminsdk-fbsvc-31174c1e26.json');
+//const keyPath = path.join(__dirname, 'keys', 'campus-guide-map-uph-firebase-adminsdk-fbsvc-31174c1e26.json');
 const identifier = process.argv[2];
 
 if (!identifier) {
   console.error('Usage: node grant-admin.js <UID_or_EMAIL>');
   process.exit(1);
 }
-
-const serviceAccount = require(keyPath);
-admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
+//
+//const serviceAccount = require(keyPath);
+//admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
 
 (async () => {
   let uid = identifier;
